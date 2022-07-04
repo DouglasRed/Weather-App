bd4f1e04f19d236ef408842f2ca892a43cedbc91
Doug Red <douglasred63@yahoo.com>
2022-07-04 14:38:53 -0500

    added weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,32 @@
-var test = function () {
-  console.log("Hello");
+var cityFormEl = document.querySelector("#city-form");
+var cityInputEl = document.querySelector("#city");
+var searches = [];
+
+//get value from input element
+
+var getWeather = function (cityName) {
+  var apiKey = "821bf60e7a4a447f19613519e829ee2c";
+  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
+
+  fetch(apiUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        console.log(data);
+      });
+    }
+  });
 };
 
-test();
+var formSubmitHandler = function (event) {
+  event.preventDefault();
+  var cityName = cityInputEl.value.trim();
+
+  if (cityName) {
+    getWeather(cityName);
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a city");
+  }
+};
+
+cityFormEl.addEventListener("submit", formSubmitHandler);
