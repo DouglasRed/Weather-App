1102892694ab942d0d4926aec5764f6a322a377a
Doug Red <douglasred63@yahoo.com>
2022-07-04 19:12:15 -0500

    added a for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,17 @@
 var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
-var searches = [];
+var weatherContainerEl = document.querySelector("#weather-container");
+var weatherSearchedCity = document.querySelector("#weather-searched-city");
+var weatherForecast = document.querySelector("#forecast-container");
+var date = new Date();
+var currentDate =
+  date.getMonth() + 1 + "/" + date.getDate() + "/" + date.getFullYear();
+var weatherTemp = document.querySelector("#weather-temp");
+var weatherWind = document.querySelector("#weather-wind");
+var weatherHumidity = document.querySelector("#weather-humidity");
+var weatherUV = document.querySelector("#weather-uv");
 
+var searches = [];
 //get value from input element
 
 var getWeather = function (cityName) {
@@ -11,8 +21,10 @@ var getWeather = function (cityName) {
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        console.log(data);
+        displayWeather(data, city);
       });
+    } else {
+      weatherSearchedCity.textContent = "Error: City not found";
     }
   });
 };
@@ -26,6 +38,77 @@ var formSubmitHandler = function (event) {
     cityInputEl.value = "";
   } else {
     alert("Please enter a city");
+  }
+};
+
+var displayWeather = function (weather, searchedCity) {
+  // weatherContainerEl.textContent = "";
+  weatherContainerEl.classList = "border";
+  weatherSearchedCity.textContent = weather.name + " (" + currentDate + ")";
+  weatherTemp.textContent = "Temp: " + weather.main.temp + `\u00B0` + "F";
+  weatherWind.textContent = "Wind: " + weather.wind.speed + " MPH";
+  weatherHumidity.textContent = "Humidity: " + weather.main.humidity + "%";
+  weatherForecast.textContent = "5-Day Forecast:";
+  // weatherUV.textContent = "" + weather.name;
+  if (weather.length === 0) {
+    weatherContainerEl.textContent = "City not found";
+    return;
+  }
+
+  for (var i = 0; i < 5; i++) {
+    var futureDay =
+      date.getMonth() +
+      1 +
+      "/" +
+      (date.getDate() + parseInt([i]) + 1) +
+      "/" +
+      date.getFullYear();
+
+    var weatherCardEl = document.createElement("div");
+    weatherCardEl.classList = "list-item col-2";
+
+    var titleEl = document.createElement("span");
+    titleEl.textContent = futureDay;
+
+    weatherCardEl.appendChild(titleEl);
+
+    var statusEl = document.createElement("span");
+    statusEl.classList = "list-item";
+
+    // if (weather.main === "Clouds") {
+    //   statusEl.innerHTML = "☁️";
+    // } else if (weather[i].main === "Thunderstorm") {
+    //   statusEl.innerHTML = "⛈️";
+    // } else if (weather[i].main === "Drizzle") {
+    //   statusEl.innerHTML = "🌧️";
+    // } else if (weather[i].main === "Rain") {
+    //   statusEl.innerHTML = "🌦️";
+    // } else if (weather[i].main === "Snow") {
+    //   statusEl.innerHTML = "❄️";
+    // } else if (
+    //   weather[i].main === "Mist" ||
+    //   "Smoke" ||
+    //   "Haze" ||
+    //   "Dust" ||
+    //   "Fog" ||
+    //   "Sand" ||
+    //   "Ash" ||
+    //   "squall" ||
+    //   "Tornado"
+    // ) {
+    //   statusEl.innerHTML = "🌫️";
+    // } else if (weather[i].main === "Clear") {
+    //   statusEl.innerHTML = "☀️";
+    // } else {
+    //   statusEl.innerHTML = "Image unavailable";
+    // }
+
+    weatherCardEl.appendChild(statusEl);
+    weatherForecast.appendChild(weatherCardEl);
+
+    console.log(futureDay);
+
+    console.log(weather, searchedCity);
   }
 };
 
